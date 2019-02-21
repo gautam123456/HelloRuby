14c0dbe513f8ecb34dfcd181a799d07f530bb9c5
Ranjeet Gautam <gautamr@vmware.com>
2019-02-21 16:25:48 +0000

    Initial commit for ruby projrct

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRuby::Application.config.secret_token = '016e8ce3f188fa1acca625eb119cdcfbf09b0df6c0db5262b3e2d886ea2a19a34945db2bca807dbacec1bda3298f38700c246502e6d1efd3a18be71bfa5d10cf'
